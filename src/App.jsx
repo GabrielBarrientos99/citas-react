e10fdd8218bbb56835000a5ec23a300ce891163d
GabrielBarrientos99 <gabriel.barrientos.c@uni.pe>
2023-12-27 18:36:59 -0500

    deploy

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import Header from "./componentes/Header"
+import Formulario from "./componentes/Formulario"
+import ListadoPaciente from "./componentes/ListadoPaciente"
+import {useState , useEffect} from 'react'
+
+function App() { 
+  
+  const [pacientes,setPacientes] = useState([])
+  const [paciente,setPaciente] = useState({})
+
+  useEffect(()=>{
+    const obtenerLS = ()=>{
+      const pacientesLS = JSON.parse(localStorage.getItem('pacientes')) ?? [];
+      console.log(typeof pacientesLS)
+      setPacientes(pacientesLS)
+    }
+    obtenerLS();
+},[])
+
+  useEffect(()=>{
+      localStorage.setItem('pacientes',JSON.stringify(pacientes));
+  },[pacientes])
+
+  const eliminarPaciente = id => {
+    const pacientesActualizados = pacientes.filter(paciente=> paciente.id!==id)
+    setPacientes(pacientesActualizados)
+  }
+
+  return (    
+    <div className="container mx-auto mt-20">
+      <Header />
+      <div className="mt-12 md:flex">
+          <Formulario 
+            pacientes={pacientes}
+            setPacientes= {setPacientes}
+            paciente = {paciente}  
+            setPaciente={setPaciente}         
+          />   
+          <ListadoPaciente 
+            pacientes={pacientes}
+            setPaciente={setPaciente}
+            eliminarPaciente={eliminarPaciente}
+          />  
+      </div>          
+    </div>
+  )
+}
+
+export default App
